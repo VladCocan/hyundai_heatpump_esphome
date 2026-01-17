6362d281c24b3d89840dcc1857b0a3f500629452
Vlad Cocan <cfvlad@gmail.com>
2026-01-18 01:05:02 +0200

    Implement Zone1 and Room power switch classes
    Added classes for Zone1 and Room power switches with setup logic.

--- components/hyundai_heatpump/hyundai_heatpump_switches.cpp
+++ components/hyundai_heatpump/hyundai_heatpump_switches.cpp
@@ -1,4 +1,35 @@
 #include "hyundai_heatpump_switches.h"
+#include "esphome/core/log.h"
 
 namespace esphome {
-namespace hyundai
+namespace hyundai_heatpump {
+
+static const char *const TAG = "hyundai_heatpump.switches";
+
+class Zone1PowerSwitch : public switch_::Switch {
+ public:
+  HyundaiHeatpump *parent_;
+  void write_state(bool state) override {
+    parent_->set_zone1_power(state);
+    publish_state(state);
+  }
+};
+
+class RoomPowerSwitch : public switch_::Switch {
+ public:
+  HyundaiHeatpump *parent_;
+  void write_state(bool state) override {
+    parent_->set_room_power(state);
+    publish_state(state);
+  }
+};
+
+void HyundaiHeatpumpSwitches::setup() {
+  ((Zone1PowerSwitch*)zone1_power)->parent_ = parent_;
+  ((RoomPowerSwitch*)room_power)->parent_ = parent_;
+
+  ESP_LOGI(TAG, "Hyundai R290 switches initialized");
+}
+
+}  // namespace hyundai_heatpump
+}  // namespace esphome
