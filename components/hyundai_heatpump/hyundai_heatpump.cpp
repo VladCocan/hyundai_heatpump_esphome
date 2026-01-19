6e9e8f28c9efac0e078fa2ff98b2c1c2a4304d8b
Vlad Cocan <cfvlad@gmail.com>
2026-01-19 20:06:39 +0200

    x

--- components/hyundai_heatpump/hyundai_heatpump.cpp
+++ components/hyundai_heatpump/hyundai_heatpump.cpp
@@ -1,14 +1,124 @@
 #include "hyundai_heatpump.h"
 
-namespace esphome {
-namespace hyundai_heatpump {
+HyundaiHeatPump::HyundaiHeatPump(uint32_t update_interval_ms)
+  : PollingComponent(update_interval_ms) {}
 
-void HyundaiHPInternalHub::setup() {
-  // Initialize Modbus or any startup logic
+// Bit helper
+bool HyundaiHeatPump::get_bit(uint16_t value, uint8_t bit) {
+  return (value >> bit) & 1;
 }
 
-void HyundaiHPInternalHub::loop() {
-  // Poll devices if needed
+// Modbus read
+uint16_t HyundaiHeatPump::read_register(uint16_t address) {
+  uint16_t value;
+  if (!this->modbus_read_holding_registers(address - 40001, 1, &value)) {
+    ESP_LOGW("HyundaiHeatPump", "Failed to read register %u", address);
+    return 0xFFFF;
+  }
+  return value;
 }
 
-}  // namespace hyundai_heatpump
+// Modbus write
+void HyundaiHeatPump::write_register(uint16_t address, uint16_t value) {
+  if (!this->modbus_write_single_register(address - 40001, value)) {
+    ESP_LOGW("HyundaiHeatPump", "Failed to write register %u", address);
+  }
+}
+
+// Setup
+void HyundaiHeatPump::setup() {
+  ESP_LOGCONFIG("HyundaiHeatPump", "Component setup done");
+}
+
+// Setters
+void HyundaiHeatPump::set_zone1_temperature(float temp) {
+  zone1_target_ = static_cast<uint16_t>(temp * 10);
+  write_register(40003, zone1_target_);
+}
+
+void HyundaiHeatPump::set_zone2_temperature(float temp) {
+  zone2_target_ = static_cast<uint16_t>(temp * 10);
+  write_register(40005, zone2_target_);
+}
+
+void HyundaiHeatPump::set_mode(uint8_t mode) {
+  mode_ = mode;
+  write_register(40002, mode_);
+}
+
+void HyundaiHeatPump::set_dhw_temperature(float temp) {
+  dhw_target_ = static_cast<uint16_t>(temp * 10);
+  write_register(40010, dhw_target_);
+}
+
+void HyundaiHeatPump::enable_dhw(bool on) {
+  uint16_t reg = read_register(40001);
+  if (reg == 0xFFFF) return;
+  if (on) reg |= (1 << 1); else reg &= ~(1 << 1);
+  write_register(40001, reg);
+}
+
+void HyundaiHeatPump::enable_tbh(bool on) {
+  uint16_t reg = read_register(40001);
+  if (reg == 0xFFFF) return;
+  if (on) reg |= (1 << 3); else reg &= ~(1 << 3);
+  write_register(40001, reg);
+}
+
+void HyundaiHeatPump::enable_ibh1(bool on) {
+  uint16_t reg = read_register(40001);
+  if (reg == 0xFFFF) return;
+  if (on) reg |= (1 << 2); else reg &= ~(1 << 2);
+  write_register(40001, reg);
+}
+
+void HyundaiHeatPump::enable_ibh2(bool on) {
+  uint16_t reg = read_register(40001);
+  if (reg == 0xFFFF) return;
+  if (on) reg |= (1 << 4); else reg &= ~(1 << 4);
+  write_register(40001, reg);
+}
+
+// Polling update
+void HyundaiHeatPump::update() {
+  // Temperatures
+  uint16_t tw_in = read_register(40103);
+  if (tw_in != 0xFFFF && tw_in_sensor_) tw_in_sensor_->publish_state(tw_in / 10.0f);
+
+  uint16_t tw_out = read_register(40104);
+  if (tw_out != 0xFFFF && tw_out_sensor_) tw_out_sensor_->publish_state(tw_out / 10.0f);
+
+  uint16_t t1 = read_register(40111);
+  if (t1 != 0xFFFF && t1_sensor_) t1_sensor_->publish_state(t1 / 10.0f);
+
+  uint16_t tw2 = read_register(40112);
+  if (tw2 != 0xFFFF && tw2_sensor_) tw2_sensor_->publish_state(tw2 / 10.0f);
+
+  uint16_t ta = read_register(40114);
+  if (ta != 0xFFFF && ta_sensor_) ta_sensor_->publish_state(ta / 10.0f);
+
+  uint16_t t5 = read_register(40115);
+  if (t5 != 0xFFFF && t5_sensor_) t5_sensor_->publish_state(t5 / 10.0f);
+
+  uint16_t t4 = read_register(40109);
+  if (t4 != 0xFFFF && t4_sensor_) t4_sensor_->publish_state(t4 / 10.0f);
+
+  // Status bits
+  uint16_t status = read_register(40129);
+  if (status != 0xFFFF) {
+    if (compressor_) compressor_->publish_state(get_bit(status, 0));
+    if (pump_i_) pump_i_->publish_state(get_bit(status, 3));
+    if (pump_d_) pump_d_->publish_state(get_bit(status, 2));
+    if (tbh_) tbh_->publish_state(get_bit(status, 0));
+    if (ibh1_) ibh1_->publish_state(get_bit(status, 0));
+    if (ibh2_) ibh2_->publish_state(get_bit(status, 1));
+  }
+
+  // Error code
+  uint16_t error = read_register(40188);
+  if (error != 0xFFFF && error_code_) {
+    char buf[16];
+    sprintf(buf, "0x%04X", error);
+    error_code_->publish_state(buf);
+  }
+}
--- components/hyundai_heatpump/hyundai_heatpump.h
+++ components/hyundai_heatpump/hyundai_heatpump.h
@@ -2,15 +2,54 @@
 #include "esphome.h"
 #include "esphome/components/modbus/modbus.h"
 
-namespace esphome {
-namespace hyundai_heatpump {
-
-class HyundaiHPInternalHub : public Component {
+class HyundaiHeatPump : public PollingComponent, public modbus::ModbusDevice {
  public:
-  void set_bus(modbus::Modbus *bus) { this->bus_ = bus; }
- protected:
-  modbus::Modbus *bus_;
-};
+  // Constructor
+  HyundaiHeatPump(uint32_t update_interval_ms = 15000);
 
-}  // namespace hyundai_heatpump
-}  // namespace esphome
+  // Climate targets
+  void set_zone1_temperature(float temp);
+  void set_zone2_temperature(float temp);
+  void set_mode(uint8_t mode);  // 1=Auto,2=Cooling,3=Heating
+
+  // DHW
+  void set_dhw_temperature(float temp);
+  void enable_dhw(bool on);
+
+  // TBH / IBH
+  void enable_tbh(bool on);
+  void enable_ibh1(bool on);
+  void enable_ibh2(bool on);
+
+  // Sensors (ESPHome objects)
+  sensor::Sensor *tw_in_sensor_{nullptr};
+  sensor::Sensor *tw_out_sensor_{nullptr};
+  sensor::Sensor *t1_sensor_{nullptr};
+  sensor::Sensor *tw2_sensor_{nullptr};
+  sensor::Sensor *ta_sensor_{nullptr};
+  sensor::Sensor *t5_sensor_{nullptr};
+  sensor::Sensor *t4_sensor_{nullptr};
+  binary_sensor::BinarySensor *compressor_{nullptr};
+  binary_sensor::BinarySensor *pump_i_{nullptr};
+  binary_sensor::BinarySensor *pump_d_{nullptr};
+  binary_sensor::BinarySensor *tbh_{nullptr};
+  binary_sensor::BinarySensor *ibh1_{nullptr};
+  binary_sensor::BinarySensor *ibh2_{nullptr};
+  text_sensor::TextSensor *error_code_{nullptr};
+
+  void setup() override;
+  void update() override;
+
+ private:
+  // Internal helpers
+  uint16_t read_register(uint16_t address);
+  void write_register(uint16_t address, uint16_t value);
+  bool get_bit(uint16_t value, uint8_t bit);
+
+  uint16_t zone1_target_{0};
+  uint16_t zone2_target_{0};
+  uint16_t dhw_target_{0};
+  uint16_t mode_{1};  // Default Auto
+
+  uint16_t last_status_register_{0};
+};
