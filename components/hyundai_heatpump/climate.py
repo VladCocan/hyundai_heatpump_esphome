fd006a2c427fe5c08075e893d400c0435a68d588
Vlad Cocan <cfvlad@gmail.com>
2026-01-19 21:36:47 +0200

    ss

--- components/hyundai_heatpump/climate.py
+++ components/hyundai_heatpump/climate.py
@@ -1,31 +1,27 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import climate, modbus_controller
+from esphome.components import climate
+from esphome.const import CONF_ID, CONF_NAME
 
-DEPENDENCIES = ["modbus_controller"]
+CODEOWNERS = ["@you"]
 
 hyundai_ns = cg.esphome_ns.namespace("hyundai_heatpump")
-
 HyundaiHPClimate = hyundai_ns.class_(
     "HyundaiHPClimate",
     climate.Climate,
     cg.Component,
 )
 
-CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend(
-    {
-        cv.GenerateID(): cv.declare_id(HyundaiHPClimate),
-        cv.Required("modbus_controller_id"): cv.use_id(
-            modbus_controller.ModbusController
-        ),
-    }
+CONFIG_SCHEMA = (
+    climate._CLIMATE_SCHEMA.extend(
+        {
+            cv.GenerateID(): cv.declare_id(HyundaiHPClimate),
+        }
+    )
+    .extend(cv.COMPONENT_SCHEMA)
 )
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[cv.GenerateID()])
-
-    parent = await cg.get_variable(config["modbus_controller_id"])
-    cg.add(var.set_modbus_parent(parent))
-
+    var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await climate.register_climate(var, config)
