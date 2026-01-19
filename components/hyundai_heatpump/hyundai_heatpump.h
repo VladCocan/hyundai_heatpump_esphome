8eb15fb91d68156a6c60712ad3dc82093e39a249
Vlad Cocan <cfvlad@gmail.com>
2026-01-19 20:12:50 +0200

    x

--- components/hyundai_heatpump/hyundai_heatpump.h
+++ components/hyundai_heatpump/hyundai_heatpump.h
@@ -1,55 +1,58 @@
 #pragma once
+
 #include "esphome.h"
 #include "esphome/components/modbus/modbus.h"
+#include "esphome/components/sensor/sensor.h"
+#include "esphome/components/binary_sensor/binary_sensor.h"
+#include "esphome/components/text_sensor/text_sensor.h"
+#include "esphome/components/climate/climate.h"
 
-class HyundaiHeatPump : public PollingComponent, public modbus::ModbusDevice {
+namespace esphome {
+namespace hyundai_heatpump {
+
+// -------------------- Hub Class --------------------
+class HyundaiHPInternalHub : public Component, public modbus::ModbusDevice {
  public:
-  // Constructor
-  HyundaiHeatPump(uint32_t update_interval_ms = 15000);
-
-  // Climate targets
-  void set_zone1_temperature(float temp);
-  void set_zone2_temperature(float temp);
-  void set_mode(uint8_t mode);  // 1=Auto,2=Cooling,3=Heating
-
-  // DHW
-  void set_dhw_temperature(float temp);
-  void enable_dhw(bool on);
-
-  // TBH / IBH
-  void enable_tbh(bool on);
-  void enable_ibh1(bool on);
-  void enable_ibh2(bool on);
-
-  // Sensors (ESPHome objects)
-  sensor::Sensor *tw_in_sensor_{nullptr};
-  sensor::Sensor *tw_out_sensor_{nullptr};
-  sensor::Sensor *t1_sensor_{nullptr};
-  sensor::Sensor *tw2_sensor_{nullptr};
-  sensor::Sensor *ta_sensor_{nullptr};
-  sensor::Sensor *t5_sensor_{nullptr};
-  sensor::Sensor *t4_sensor_{nullptr};
-  binary_sensor::BinarySensor *compressor_{nullptr};
-  binary_sensor::BinarySensor *pump_i_{nullptr};
-  binary_sensor::BinarySensor *pump_d_{nullptr};
-  binary_sensor::BinarySensor *tbh_{nullptr};
-  binary_sensor::BinarySensor *ibh1_{nullptr};
-  binary_sensor::BinarySensor *ibh2_{nullptr};
-  text_sensor::TextSensor *error_code_{nullptr};
-
-  void setup() override;
+  void setup() override {}
   void update() override;
 
- private:
-  // Internal helpers
-  uint16_t read_register(uint16_t address);
-  void write_register(uint16_t address, uint16_t value);
-  bool get_bit(uint16_t value, uint8_t bit);
+  // Temperature sensors
+  sensor::Sensor *tw_in_sensor{nullptr};
+  sensor::Sensor *tw_out_sensor{nullptr};
+  sensor::Sensor *t1_sensor{nullptr};
+  sensor::Sensor *tw2_sensor{nullptr};
+  sensor::Sensor *ta_sensor{nullptr};
+  sensor::Sensor *t5_sensor{nullptr};
+  sensor::Sensor *t4_sensor{nullptr};
 
-  uint16_t zone1_target_{0};
-  uint16_t zone2_target_{0};
-  uint16_t dhw_target_{0};
-  uint16_t mode_{1};  // Default Auto
+  // Binary sensors
+  binary_sensor::BinarySensor *compressor{nullptr};
+  binary_sensor::BinarySensor *pump_i{nullptr};
+  binary_sensor::BinarySensor *pump_d{nullptr};
+  binary_sensor::BinarySensor *tbh{nullptr};
+  binary_sensor::BinarySensor *ibh1{nullptr};
+  binary_sensor::BinarySensor *ibh2{nullptr};
 
-  uint16_t last_status_register_{0};
+  // Text sensor
+  text_sensor::TextSensor *error_code{nullptr};
+
+  // Read registers via Modbus
+  float read_register(uint16_t reg);
 };
+
+// -------------------- Climate Class --------------------
+class HyundaiHPInternalClimate : public climate::Climate {
+ public:
+  void set_hub(HyundaiHPInternalHub *hub) { this->hub_ = hub; }
+
+  // Required implementations
+  climate::ClimateTraits traits() override;
+  void control(const climate::ClimateCall &call) override;
+  void update() override;
+
+ protected:
+  HyundaiHPInternalHub *hub_;
+};
+
+}  // namespace hyundai_heatpump
+}  // namespace esphome
