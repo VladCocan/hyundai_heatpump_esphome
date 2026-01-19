f9253baa5ce697a3ca8d51d5c3bddadd73a91081
Vlad Cocan <cfvlad@gmail.com>
2026-01-19 20:54:50 +0200

    z

--- components/hyundai_heatpump/climate.py
+++ components/hyundai_heatpump/climate.py
@@ -14,11 +14,15 @@ CONFIG_SCHEMA = climate._CLIMATE_SCHEMA.extend({
     cv.Required(CONF_MODBUS_ID): cv.use_id(modbus_controller.ModbusController),
 })
 
+HyundaiHP = cg.esphome_ns.class_(
+    "HyundaiHP", climate.Climate, cg.Component
+)
+
 async def to_code(config):
     parent = await cg.get_variable(config[CONF_MODBUS_ID])
     
     
-    var = cg.new_Pvariable(config[CONF_ID])
+    var = cg.new_Pvariable(config[cv.ID], parent)
     await cg.register_component(var, config)
     await climate.register_climate(var, config)
     cg.add(var.set_modbus_parent(parent))
