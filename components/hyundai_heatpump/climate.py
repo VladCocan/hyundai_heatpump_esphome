50b57bde8b49d9562caaadec485f361cbdfb21fb
Vlad Cocan <cfvlad@gmail.com>
2026-01-19 21:14:02 +0200

    sdf

--- components/hyundai_heatpump/climate.py
+++ components/hyundai_heatpump/climate.py
@@ -1,26 +1,23 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import sensor, modbus_controller
-from .const import *
+from esphome.components import climate, modbus_controller
 
 DEPENDENCIES = ["modbus_controller"]
 
 hyundai_ns = cg.esphome_ns.namespace("hyundai_heatpump")
-HyundaiHPSensor = hyundai_ns.class_(
-    "HyundaiHPSensor", cg.PollingComponent, sensor.Sensor
+HyundaiHPClimate = hyundai_ns.class_(
+    "HyundaiHPClimate", climate.Climate, cg.Component
 )
 
 CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(HyundaiHPSensor),
+    cv.GenerateID(): cv.declare_id(HyundaiHPClimate),
     cv.Required("modbus_controller_id"): cv.use_id(modbus_controller.ModbusController),
-    cv.Optional("name", default="Hyundai Water Temp"): cv.string,
-}).extend(sensor._SENSOR_SCHEMA)
+    cv.Optional("name", default="Hyundai Heat Pump"): cv.string,
+}).extend(climate._CLIMATE_SCHEMA)  # folosim _CLIMATE_SCHEMA, nu CLIMATE_SCHEMA
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[cv.GenerateID()])  # aici nu mai folosim CONF_ID
-
+    var = cg.new_Pvariable(config[cv.GenerateID()])  # obținem instanța corectă
     parent = await cg.get_variable(config["modbus_controller_id"])
     cg.add(var.set_modbus_parent(parent))
-
     await cg.register_component(var, config)
-    await sensor.register_sensor(var, config)
+    await climate.register_climate(var, config)
