4f61cfc1a90b25237508e31fce179798ad4ad397
Vlad Cocan <cfvlad@gmail.com>
2026-01-19 21:12:26 +0200

    sd

--- components/hyundai_heatpump/climate.py
+++ components/hyundai_heatpump/climate.py
@@ -1,37 +1,26 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import climate, modbus_controller
-from esphome.const import CONF_NAME
-
+from esphome.components import sensor, modbus_controller
 from .const import *
 
 DEPENDENCIES = ["modbus_controller"]
 
-CONF_MODBUS_ID = "modbus_controller_id"
-
-# Creează namespace-ul ESPHome pentru componenta ta
 hyundai_ns = cg.esphome_ns.namespace("hyundai_heatpump")
-
-# Creează clasa Python care va fi generată în C++
-HyundaiHPClimate = hyundai_ns.class_(
-    "HyundaiHPClimate", climate.Climate, cg.Component
+HyundaiHPSensor = hyundai_ns.class_(
+    "HyundaiHPSensor", cg.PollingComponent, sensor.Sensor
 )
 
-# Schema YAML
 CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(HyundaiHPClimate),  # aici se generează ID
-    cv.Required(CONF_MODBUS_ID): cv.use_id(modbus_controller.ModbusController),
-    cv.Optional(CONF_NAME, default="Hyundai Heat Pump"): cv.string,
-}).extend(climate._CLIMATE_SCHEMA)
+    cv.GenerateID(): cv.declare_id(HyundaiHPSensor),
+    cv.Required("modbus_controller_id"): cv.use_id(modbus_controller.ModbusController),
+    cv.Optional("name", default="Hyundai Water Temp"): cv.string,
+}).extend(sensor._SENSOR_SCHEMA)
 
 async def to_code(config):
-    # Folosește ID-ul generat de cv.GenerateID()
-    var = cg.new_Pvariable(config[cv.GenerateID()])  # NU mai folosi cv.ID
+    var = cg.new_Pvariable(config[cv.GenerateID()])  # aici nu mai folosim CONF_ID
 
-    # Conectează la modbus_controller
-    parent = await cg.get_variable(config[CONF_MODBUS_ID])
+    parent = await cg.get_variable(config["modbus_controller_id"])
     cg.add(var.set_modbus_parent(parent))
 
-    # Înregistrează componenta ESPHome
     await cg.register_component(var, config)
-    await climate.register_climate(var, config)
+    await sensor.register_sensor(var, config)
